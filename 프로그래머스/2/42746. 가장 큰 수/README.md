c00d2496f01facab9664b38f4777b8a865d36b3a
이승구 <jeomjang2@gmail.com>
2026-08-13 15:38:43 +0900

    [level 2] Title: 가장 큰 수, Time: 105.70 ms, Memory: 134 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/README.md
+++ 프로그래머스/2/42746. 가장 큰 수/README.md
@@ -1,10 +1,10 @@
 # [level 2] 가장 큰 수 - 42746 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42746') 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42746) 
 
 ### 성능 요약
 
-메모리: 401 MB, 시간: 1537.70 ms
+메모리: 134 MB, 시간: 105.70 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 08월 13일 15:23:35
+2026년 08월 13일 15:38:41
 
 ### 문제 설명
 
--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -13,14 +13,20 @@ class Solution {
         Arrays.sort(stringNumbers, (a,b)->(b+a).compareTo(a+b));
         // 3 30 vs 30 3
         
+        StringBuilder sb = new StringBuilder();
+        
         for(int i=0; i<numbers.length; i++){
-            answer += stringNumbers[i];
+            sb.append(stringNumbers[i]);
+            //answer += stringNumbers[i];
             //System.out.print(stringNumbers[i]+" ");
         }
+        
+        answer = sb.toString();
         
         if(answer.charAt(0) == '0'){
             answer = "0";
         }
+        
         
         
         return answer;
