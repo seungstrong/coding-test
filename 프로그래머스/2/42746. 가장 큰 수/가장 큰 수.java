c7f742ac9c26878270305ac0055cdfafbdbdb32b
이승구 <jeomjang2@gmail.com>
2026-08-13 15:23:39 +0900

    [level 2] Title: 가장 큰 수, Time: 1537.70 ms, Memory: 401 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class Solution {
+    public String solution(int[] numbers) {
+        String answer = "";
+        
+        String[] stringNumbers = new String[numbers.length];
+        
+        for(int i=0; i<numbers.length; i++){
+            stringNumbers[i] = String.valueOf(numbers[i]);
+        }
+        
+        Arrays.sort(stringNumbers, (a,b)->(b+a).compareTo(a+b));
+        // 3 30 vs 30 3
+        
+        for(int i=0; i<numbers.length; i++){
+            answer += stringNumbers[i];
+            //System.out.print(stringNumbers[i]+" ");
+        }
+        
+        if(answer.charAt(0) == '0'){
+            answer = "0";
+        }
+        
+        
+        return answer;
+    }
+}
+
+/*
+a.compareTo(b)
+음수 -> a를 앞으로
+0 -> 같음
+양수 -> a를 뒤로
+*/
