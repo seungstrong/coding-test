886a7fc183f1f535641afe84479de8eab2c983cc
이승구 <jeomjang2@gmail.com>
2026-08-24 00:14:11 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 63.24 ms, Memory: 122 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -4,20 +4,20 @@ class Solution {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
         
-        // 1. 해시맵을 만든다.
+        // 1. 참가자별 등장 횟수를 해시맵에 저장
         HashMap<String, Integer> map = new HashMap<>();
         for(String player : participant)
         {
             map.put(player, map.getOrDefault(player, 0) + 1);
         }
         
-        // 2. 해시맵을 뺀다
+        // 2. 완주한 선수의 등장 횟수를 1씩 감소
         for(String player : completion)
         {
             map.put(player, map.get(player) - 1);
         }
         
-        // 3. value 가 0이 아닌 원소를 찾는다.
+        // 3. value 가 0이 아닌 원소(미완주자)를 찾는다.
         for(String key : map.keySet())
         {
             if(map.get(key) != 0)
